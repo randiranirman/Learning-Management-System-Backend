fe02d71767eece91ffc8b74159ed2b96831a2c33
ChanukaDasun <chanuka101@gmail.com>
2025-03-09 22:58:48 +0530

    create CourseManagement service folder structure

---
+++ CourseManagementService/Controllers/SubjectsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CourseManagementService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SubjectsController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetAllSubjects()
+        {
+            var subjectsDomainModel = new
+            {
+                Id = 1,
+                Name = "subject 1",
+                AssignDate = new DateOnly()
+            };
+
+            return Ok(subjectsDomainModel);
+        }
+    }
+}
