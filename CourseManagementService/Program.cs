ff275b47083697a8278d7368897ee66ace92f045
ChanukaDasun <chanuka101@gmail.com>
2025-03-21 16:00:03 +0530

    Complete all the CRUD operations for manage courses in admin role

--- CourseManagementService/Program.cs
+++ CourseManagementService/Program.cs
@@ -31,7 +31,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy(name: MyAllowSpecificOrigins,
         policy =>
         {
-            policy.WithOrigins("http://localhost:5174") // Allow frontend URL
+            policy.WithOrigins("http://localhost:5173") // Allow frontend URL
                   .AllowAnyMethod()
                   .AllowAnyHeader();
         });
